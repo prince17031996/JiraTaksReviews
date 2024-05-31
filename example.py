7900a759809d840ca7e6fe301921e518f3c320ac
Princejohnsonandjohnson <PRaj7@its.jnj.com>
2024-05-31 18:11:33 +0530

    Please add this new change commit

--- example.py
+++ example.py
@@ -9,7 +9,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from Pages.basePage import Basepage
 
 driver = webdriver.Chrome()
-driver.get("https://jira.jnj.com/browse/JCVZ-932")
+driver.get("https://jira.jnj.com/browse/JCVZ-1030")
 #https://jira.jnj.com/browse/JCVZ-1030
 driver.maximize_window()
 driver.implicitly_wait(10)
@@ -48,20 +48,24 @@ for x in range(1,len(data)+1):
 
     except NoSuchElementException:
         break"""""
-l = list()
-data = driver.find_elements(By.XPATH,"(//div[@data-testid='Action-view']//child::div[@class='test-step-field-content'])")
-print(len(data))
+#l = list()
+#data = driver.find_elements(By.XPATH,"(//div[@data-testid='Action-view']//child::div[@class='test-step-field-content'])")
+#print(len(data))
 
 scroll_amount = 180
 pixel=0
-for x in range(1, len(data) + 1):
+#  test data insdex //div[@id='test-step-index-10']
+# //div[@data-index='9']
+#parent //div[@data-index='9']//div[@class='step-container sc-CtfFt fiKMAr']//div[@class='step-content sc-laTMn ijLfRy']//div[@class='step-fields sc-hGoxap bDhqzx']
+#//div[@data-index='9']//div[@class='step-container sc-CtfFt fiKMAr']//div[@class='step-content sc-laTMn ijLfRy']//div[@class='step-fields sc-hGoxap bDhqzx']//div[@class='text-field-holder field-holder sc-itybZL jtGvFH sc-fjmCvl lmUaVJ']//div[@class='text-field-container sc-iRbamj bhOdCd']//div[@id='field_NATIVE_STEP_NATIVE_33732_7480306']//div[@data-testid='Action-view']//div[@class='test-step-field-content']
+for x in range(0, 20):
     try:
-        z = driver.find_element(By.XPATH,
-                                f"(//div[@data-testid='Action-view']//child::div[@class='test-step-field-content'])[{x}]")
+        z = driver.find_element(By.XPATH,f"//div[@data-index='{x}']//div[@class='step-container sc-CtfFt fiKMAr']//div[@class='step-content sc-laTMn ijLfRy']//div[@class='step-fields sc-hGoxap bDhqzx']//div[@class='text-field-holder field-holder sc-itybZL jtGvFH sc-fjmCvl lmUaVJ']//div[@class='text-field-container sc-iRbamj bhOdCd']//div[@tabindex='-1']//div[@data-testid='Action-view']//div[@class='test-step-field-content']")
+        time.sleep(1)
         if z:
             print(z.text)
         # Scroll down by dynamically increasing pixels using JavaScript
-        #driver.execute_script("arguments[0].scrollIntoView();",z)
+        driver.execute_script("arguments[0].scrollIntoView();",z)
 
         time.sleep(1)
         # Add a small delay to allow content to load
